ef75092a0e20d53b72fddaba115fe359dde42fa1
Haley <haleylgoins@gmail.com>
2021-10-09 18:48:00 -0400

    cleaned up script code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,28 @@
-let buttonEl = document.querySelector('#save-task');
+let formEl = document.querySelector('#task-form');
 let tasksToDoEl = document.querySelector('#tasks-to-do');
 
-let createTaskHandler = function() {
+let taskFormHandler = function(event) {
+
+    event.preventDefault();
+
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // Create list item
     let listItemEl = document.createElement('li');
     listItemEl.className = 'task-item';
-    listItemEl.textContent = 'This is a new task.';
+
+    // Create div to hold task info and add to list item
+    let taskInfoEl = document.createElement('div');
+    // Give it a class name
+    taskInfoEl.className = 'task-info';
+
+    // Add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    // Add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener('click', createTaskHandler);
+formEl.addEventListener('submit', taskFormHandler);
