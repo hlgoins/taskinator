786f9c84300bd9987584a66c59b59338770ee75c
Haley <haleylgoins@gmail.com>
2021-10-09 20:31:06 -0400

    complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,26 @@ let taskFormHandler = function(event) {
     let taskNameInput = document.querySelector("input[name='task-name']").value;
     let taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-    // Create list item
+    // Check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+        alert('You need to fill out the task form!');
+        return false;
+    }
+
+    formEl.reset();
+
+    // Package up data as an object
+    let taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    // Send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+    };
+
+    let createTaskEl = function (taskDataObj) {
+        // Create list item
     let listItemEl = document.createElement('li');
     listItemEl.className = 'task-item';
 
@@ -16,9 +35,8 @@ let taskFormHandler = function(event) {
     let taskInfoEl = document.createElement('div');
     // Give it a class name
     taskInfoEl.className = 'task-info';
-
     // Add HTML content to div
-    taskInfoEl.innerHTML = "<h3 class='task-name>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);
 
     // Add entire list item to list
